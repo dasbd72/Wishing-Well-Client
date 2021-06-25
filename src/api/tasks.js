4da1734834ce22373a8c8f19f855b6d045094b30
林劭軒 <twbd723@gmail.com>
2021-06-26 01:02:43 +0800

    Api task update

--- src/api/tasks.js
+++ src/api/tasks.js
@@ -1,8 +1,15 @@
 import axios from "axios";
 import { baseUrl } from "Api/backend";
 
-const tasksBaseUrl = baseUrl + "tasks";
+const tasksBaseUrl = baseUrl + "/tasks";
 
+/**
+ *
+ * @param {number} roomId
+ * @param {string} userId
+ * @param {number} selectActive 0:not confirmed, 1:rejected, 2:undone
+ * @returns
+ */
 export function listTasks(roomId, userId, selectActive = 0) {
   let url = tasksBaseUrl;
   let query = [];
@@ -21,7 +28,24 @@ export function listTasks(roomId, userId, selectActive = 0) {
   });
 }
 
-export function createTask(roomId, type, taskName, deadLine, targetPoints, userId) {
+/**
+ *
+ * @param {number} roomId
+ * @param {string} type
+ * @param {number} taskName
+ * @param {string} deadLine
+ * @param {number} targetPoints
+ * @param {string} userId
+ * @returns
+ */
+export function createTask(
+  roomId,
+  type,
+  taskName,
+  deadLine,
+  targetPoints,
+  userId
+) {
   let url = tasksBaseUrl;
 
   console.log(`Making POST request to: ${url}`);
@@ -52,43 +76,36 @@ export function AorRTask(taskId, isAccepted) {
 
   console.log(`Making POST request to: ${url}`);
 
-  return axios
-    .post(url)
-    .then((res) => {
-      if (res.status !== 200)
-        throw new Error(`Unexpected response code: ${res.status}`);
+  return axios.post(url).then((res) => {
+    if (res.status !== 200)
+      throw new Error(`Unexpected response code: ${res.status}`);
 
-      return res.data;
-    });
+    return res.data;
+  });
 }
 
 export function childrenComplete(taskId) {
-  let url = tasksBaseUrl + (`/childrenDone/${taskId}`);
- 
+  let url = tasksBaseUrl + `/childrenDone/${taskId}`;
+
   console.log(`Making POST request to: ${url}`);
 
-  return axios
-    .post(url)
-    .then((res) => {
-      if (res.status !== 200)
-        throw new Error(`Unexpected response code: ${res.status}`);
+  return axios.post(url).then((res) => {
+    if (res.status !== 200)
+      throw new Error(`Unexpected response code: ${res.status}`);
 
-      return res.data;
-    });
+    return res.data;
+  });
 }
 
 export function parentVerify(taskId) {
-  let url = tasksBaseUrl + (`/parentDone/${taskId}`);
+  let url = tasksBaseUrl + `/parentDone/${taskId}`;
 
- 
   console.log(`Making POST request to: ${url}`);
 
-  return axios
-    .post(url)
-    .then((res) => {
-      if (res.status !== 200)
-        throw new Error(`Unexpected response code: ${res.status}`);
+  return axios.post(url).then((res) => {
+    if (res.status !== 200)
+      throw new Error(`Unexpected response code: ${res.status}`);
 
-      return res.data;
-    });
+    return res.data;
+  });
 }
