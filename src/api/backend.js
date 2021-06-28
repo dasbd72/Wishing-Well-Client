51a5e7b1fac6162062a0e85aced579e0b6ffe619
林劭軒 <twbd723@gmail.com>
2021-06-28 14:26:24 +0800

    Update Backend

--- src/api/backend.js
+++ src/api/backend.js
@@ -1,2 +1,3 @@
-export const baseUrl = "http://localhost:7070/api";
-// export const baseUrl = "http://wishingwell.us-east-1.elasticbeanstalk.com/api";
+// export const baseUrl = "http://localhost:7070/api";
+export const baseUrl =
+  "http://wishing-well-server-dev.us-east-1.elasticbeanstalk.com/api";
